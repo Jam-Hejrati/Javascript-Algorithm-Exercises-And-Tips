332aa8aa621a1b8e5a95ede1c66de3bd9a911a88
jam <jamhejratipro@gmail.com>
2022-07-29 23:04:51 +0430

    Done

--- romanNumber.js
+++ romanNumber.js
@@ -2,8 +2,6 @@
 "use strict";
 
 const romanToInteger = (str) => {
-    let romanNums = str.split("");
-    let intSum = 0;
     const romanObj = {
         I: 1,
         V: 5,
@@ -13,10 +11,27 @@ const romanToInteger = (str) => {
         D: 500,
         M: 1000,
     };
+    const romanNums = str.split("");
+    let intSum = 0;
     for (let i = 0; i < romanNums.length; i++) {
-        intSum += romanObj[romanNums[i]];
+        if (
+            (romanNums[i] === "I" && romanNums[i + 1] === "V") ||
+            (romanNums[i] === "I" && romanNums[i + 1] === "X") ||
+            (romanNums[i] === "X" && romanNums[i + 1] === "L") ||
+            (romanNums[i] === "X" && romanNums[i + 1] === "C") ||
+            (romanNums[i] === "C" && romanNums[i + 1] === "D") ||
+            (romanNums[i] === "C" && romanNums[i + 1] === "M")
+        ) {
+            let temp = romanObj[romanNums[i + 1]] - romanObj[romanNums[i]];
+            intSum += temp;
+            i++;
+        } else {
+            intSum += romanObj[romanNums[i]];
+        }
     }
     return intSum;
 };
 
-console.log(romanToInteger("XII"));
+console.log(romanToInteger("III")); // 1 + 1 + 1 ~~> 3
+console.log(romanToInteger("LVIII")); // 50 + 5 + 3 ~~> 58
+console.log(romanToInteger("MCMXCIV")); // 1000 + 900 + 90 + 4 ~~> 1994

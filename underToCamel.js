ea6da99347c403d6779ff4f90e8f8ad1b3a380c8
Jam Hejrati <jamhejratipro@gmail.com>
2022-08-08 23:51:42 +0430

    unrefactored , done

--- underToCamel.js
+++ underToCamel.js
@@ -1,2 +1,19 @@
 'use strict'
 
+/* A simple programme to change under_score strings into camelCase strings */
+/* exp: test_string ~~> testString */
+
+const swapString = str => {
+    const splitedStr = str.split('_')
+    const camelArr = []
+    for(let i=1 ; i!==splitedStr.length ; i++){
+        camelArr.push(
+            splitedStr[i].replace(splitedStr[i][0] ,splitedStr[i][0].toUpperCase() )
+        )
+    }
+    camelArr.unshift(splitedStr[0])
+    return camelArr.join('')
+}
+
+console.log(swapString('jam_hejrati'))
+console.log(swapString('this_is_a_test_string'))
